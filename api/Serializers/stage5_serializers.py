8b9f2911beda084f4451af2353c8c390cdde90ec
Nirav-Lad <nirav20267@gmail.com>
2026-03-22 00:32:50 +0530

    Create and get api for submissions

--- api/Serializers/stage5_serializers.py
+++ api/Serializers/stage5_serializers.py
@@ -1,28 +1,41 @@
 from rest_framework.serializers import ValidationError,ModelSerializer
 from ..models import SubmissionWindow
 
-class SubmissionWindowSerializer(ModelSerializer):
+# POST request serailizer - for creating a submission
+class SubmissionWindowCreateSerializer(ModelSerializer):
     
     class Meta:
         model=SubmissionWindow
         fields=[
-            'id',
             'name',
             'description',
             'batch',
             'submission_start',
             'submission_end',
-            'is_active',
-            'created_by',
-            'created_on'
         ]
-        read_only_fields=['id','created_by','created_on']
 
     def validate(self, data):
         start=data.get('submission_start')
         end=data.get('submission_end')
 
-        if start>=end:
+        if start and end and start>=end:
             raise ValidationError("Submission end time must be greater than start time")
         
+        return data
+
+# GET request serializer - for reading the submissions
+class SubmissionWindowReadSerializer(ModelSerializer):
+
+    class Meta:
+        model=SubmissionWindow
+        fields="__all__"
+
+    def to_representation(self, instance):
+        data=super().to_representation(instance)
+        user=self.context.get('request').user
+
+        if user.usertype == 'Student':
+            data.pop('created_by',None)
+            data.pop('created_on',None)
+
         return data
--- api/Views/stage5_views.py
+++ api/Views/stage5_views.py
@@ -1,23 +1,73 @@
-from rest_framework.viewsets import ModelViewSet
-from ..models import SubmissionWindow
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from ..utils.permissions import IsAdminUser,IsGuideUser,IsStudentUser
-from ..serializers import SubmissionWindowSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from ..models import SubmissionWindow,StudentBatch,Batch
+from api.serializers import stage5_serializers
 
-class SubmissionWindowViewSet(ModelViewSet):
-    queryset=SubmissionWindow.objects.all().order_by('-created_on')
-    serializer_class=SubmissionWindowSerializer
+class SubmissionWindowAPIView(APIView):
     permission_classes=[IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user.id)
+    def post(self,request):
+
+        if request.user.usertype not in ["Admin","Guide"]:
+            return Response({"message":"Only Admin or Guide can create the Submission."},status=status.HTTP_403_FORBIDDEN)
+        
+        serializer=stage5_serializers.SubmissionWindowCreateSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        submission=serializer.save(
+            created_by=request.user,
+            is_active=True
+        )
+
+        return Response({
+            "message":"Submission Created successfully"
+        },status=status.HTTP_201_CREATED)
     
-    def get_queryset(self):
-        queryset=super().get_queryset()
-        batch_id=self.request.query_params.get('batch_id')
-        created_by=self.request.user.id
+    def get(self, request):
+        user = request.user
+        if user.usertype in ["Admin", "Guide"]:
+            batch_id = request.query_params.get('batch_id')
 
-        if batch_id and created_by:
-            queryset=queryset.filter(batch_id=batch_id,created_by=created_by)
+            if not batch_id:
+                return Response(
+                    {"message": "batch_id is required"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
 
-        return queryset
+            queryset = SubmissionWindow.objects.filter(batch_id=batch_id)
+
+        elif user.usertype == "Student":
+            student_batch = StudentBatch.objects.filter(enrollment_id=user.enrollment_id).first()
+            batch=Batch.objects.filter(batch_id=student_batch.current_batch_id).first()
+
+            if not batch:
+                return Response(
+                    {"message": "Student is not assigned to any batch"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            queryset = SubmissionWindow.objects.filter(
+                batch=batch,
+                is_active=True
+            )
+
+        else:
+            return Response(
+                {"message": "Invalid role"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        serializer = stage5_serializers.SubmissionWindowReadSerializer(
+            queryset,
+            many=True,
+            context={'request': request}
+        )
+
+        return Response({
+            "count": queryset.count(),
+            "data": serializer.data
+        }, status=status.HTTP_200_OK)
